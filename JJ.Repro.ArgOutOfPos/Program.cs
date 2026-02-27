bccaea81454932b3ddfc9b999b6d643a45fa1867
jjvanzon <jjvanzon79@gmail.com>
2026-02-27 22:55:17 +0100

    👩‍🔬 Add several syntaxes calling 1st overload. Simplify method implementations. Disable unrelated ReSharper warnings. Check with ReSharper unloaded error disappears.

--- JJ.Repro.ArgOutOfPos/Program.cs
+++ JJ.Repro.ArgOutOfPos/Program.cs
@@ -1,26 +1,30 @@
-﻿// Works (1st overload)
+﻿// ReSharper disable UnusedParameter.Global
+// ReSharper disable MethodOverloadWithOptionalParameter
+
+// Works (1st overload)
 bool a = "B \t".In(caseMatters: false, spaceMatters: false, [ "A", "B", "C" ]);
-bool b = "B \t".In(             false, spaceMatters: false, [ "A", "B", "C" ]);
 bool c = "B \t".In(caseMatters: false,               false, [ "A", "B", "C" ]);
+bool b = "B \t".In(             false, spaceMatters: false, [ "A", "B", "C" ]);
 bool d = "B \t".In(             false,               false, [ "A", "B", "C" ]);
 
 // Parameters swapped:
 
-// IDE => 1st overload (error)
-// Runtime => 2nd overload (works)
+// Compiler/runtime         => 2nd overload (works)
+// IDE (ReSharper disabled) => 2nd overload (no error)
+// IDE (ReSharper enabled)  => 1st overload (error)
 bool e = "B \t".In(spaceMatters: false, caseMatters: false, [ "A", "B", "C" ]);
 
 static class Impl
 {
     public static bool In(this string? value, bool caseMatters, bool spaceMatters, params IEnumerable<string?>? coll)
     {
-        return value != default && caseMatters && spaceMatters && coll != default;
+        return false;
     }
 
     // Parameter names swapped
     public static bool In(this string? value, bool spaceMatters, bool caseMatters, IEnumerable<string?>? coll,
         object? overload = default)
     {
-        return value != default && caseMatters && spaceMatters && coll != default && overload != default;
+        return true;
     }
 }
